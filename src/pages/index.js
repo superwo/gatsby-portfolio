6f8d7f229a46513cf891def37df3497f0ecbf60a
avadarad <avadanii@tiscali.it>
2020-11-01 11:02:41 +0200

    projects page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,13 +7,42 @@ import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
 
-export default () => {
+export default ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
+
   return (
     <Layout>
       <Hero />
       <Services />
       <Jobs />
+      <Projects projects={projects} title="featured projects" showLink />
     </Layout>
   )
 }
-// ...GatsbyImageSharpFluid
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        github
+        id
+        title
+        url
+        description
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
