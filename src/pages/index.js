73de68cbcdc5ed85604a8ea6add3ad8d94378080
Radu <avadanii@tiscali.it>
2021-01-02 20:27:40 +0200

    first components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
-import { graphql } from "gatsby"
+import {
+  graphql
+} from "gatsby"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import Services from "../components/Services"
@@ -7,6 +9,6 @@ import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
 export default () => {
-  return <h2>index page</h2>
+  return <Layout> <Hero /> </Layout>
 }
 // ...GatsbyImageSharpFluid
