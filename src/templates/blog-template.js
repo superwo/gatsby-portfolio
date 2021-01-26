3ec6d57336a97e6fbcff66271a2c5945fd6ec297
Avadanii Radu <radu.avadanii@bludelego.it>
2021-01-26 14:42:02 +0200

    some changes

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -3,8 +3,21 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/Layout"
 import ReactMarkdown from "react-markdown"
 
-const ComponentName = () => {
-  return <h2>blog post template</h2>
+const ComponentName = ({ data }) => {
+  // const { content } = data.blog;
+  console.log(data);
+
+  return (
+    <Layout>
+      <section className="blog-template">
+        <div className="section-center">
+          <article className="blog-content">
+            {/* {content} */}
+          </article>
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
 // export const query = graphql`
